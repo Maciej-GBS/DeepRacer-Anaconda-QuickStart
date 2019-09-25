c3272b2ed95180387ebb0e14c1bc046a2cd86a2b
Maciej Nalepa <12584491+Maciej-GBS@users.noreply.github.com>
2019-09-25 11:43:30 +0200

    Update robo.sh

--- CMD/robo.sh
+++ CMD/robo.sh
@@ -9,5 +9,5 @@ cd ..
 #docker run --rm --name dr --env-file ./robomaker.env --network sagemaker-local -p 8080:5900 -it crr0004/deepracer_robomaker:console
 
 # With log access (keep container up)
-docker run --rm --name dr --env-file ./robomaker.env --network sagemaker-local -p 8080:5900 -v $(pwd)/simulation/aws-robomaker-sample-application-deepracer/simulation_ws/src:/app/robomaker-deepracer/simulation_ws/src -v $(pwd)/simjob:/root/.ros/ -it crr0004/deepracer_robomaker:console "./run.sh build distributed_training.launch"
+docker run --rm --name dr --env-file ./robomaker.env --network sagemaker-local -p 8080:5900 -v $(pwd)/simulation/aws-robomaker-sample-application-deepracer/simulation_ws/src:/app/robomaker-deepracer/simulation_ws/src -v $(pwd)/robo/job:/root/.ros/ -it crr0004/deepracer_robomaker:console "./run.sh build distributed_training.launch"
 
